f9b105a25d006d25e9c3899e573f6797d30bbe8e
andreibojor <bojor.andrei@gmail.com>
2024-03-05 14:41:32 +0200

    asdasdsadas

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'avatars.githubusercontent.com',
-      'images.unsplash.com',
-      'lh3.googleusercontent.com',
+    remotePatterns: [
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
     ],
   },
 };
